30e543c5926f6481917929d366b7030ddfacf0c6
Mathis <mathis.ndiaye@lilo.org>
2019-04-20 15:02:46 +0200

    Premier commit du projet

---
+++ lib/src/resources/repository.dart
@@ -0,0 +1,45 @@
+import 'news_app_provider.dart';
+import 'news_db_provider.dart';
+import '../models/item_model.dart';
+
+class Repository {
+  List<Source> sources = <Source>[
+    NewsApiProvider(),
+    newsDbProvider,
+  ];
+
+  List<Cache> caches = <Cache>[
+    newsDbProvider,
+  ];
+
+  // Not implemented in newsDbProvider
+  Future<List<int>> fetchTopIds() {
+    return sources[1].fetchTopIds();
+  }
+
+  Future<ItemModel> fetchItem(int id) async{
+    Source source;
+    ItemModel item;
+    for (source in sources) {
+      item = await source.fetchItem(id);
+      if (item != null) {
+        break;
+      }
+    }
+
+    for (var cache in caches) {
+      cache.addItem(item);
+    }
+
+    return item;
+  }
+}
+
+abstract class Source {
+  Future<List<int>> fetchTopIds();
+  Future<ItemModel> fetchItem(int id);
+}
+
+abstract class Cache {
+  Future<int> addItem(ItemModel item);
+}
